58d9098d81aaeb44faf1c5f4f8703d064a075cd6
mounika1017 <24331a05j7@mvgrce.edu.in>
2025-12-12 16:22:27 +0530

    Add files via upload

---
+++ datatypes.py
@@ -0,0 +1,12 @@
+a=10
+b=4.6
+char_a='a'
+d="abcd"
+print("Integer value:", a)
+print(type(a),"=",a)
+print("Float value:", b)
+print(type(b),"=",b)
+print("character value:",char_a)
+print(type(char_a),"=",char_a)
+print("string value:","=",d)
+print(type(d),"=",d)
